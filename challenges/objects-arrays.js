6f2fa95ea414ff81be0092c3ffcc5f79f211329b
Kevin Schoell <schoell411@gmail.com>
2019-09-20 13:04:09 -0500

    final commit before lunch

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -91,7 +91,12 @@ graduates.forEach(function (info){
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+let unisWithUni = [];
+for (i=0; i < graduates.length-1; i++){
+  if (graduates[i].university.includes('uni')){
+    graduates.forEach(info => unisWithUni.push(info.university))
+}
+
 console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
